7ad91b9505fb91b9d31164372e27580c7b9eabc5
Fernando <fernandoperezverdejo@gmail.com>
2023-03-21 12:43:54 -0300

    login

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-var base_login = [ {usuario:"admin", contraseña:"1234"} , {usuario:"user",contraseña:"1234"}]
+var base_login = [ {usuario:"admin", contraseña:"1234"} , {usuario:"user",contraseña:"12345"}]
 var switch_login=false;
 
 function login(){
     var user=document.getElementById('floatingInput1').value;
     var pass=document.getElementById('floatingPassword1').value;
-    
-    if (user == base_login[0].usuario && pass == base_login[0].contraseña){
-        alert('Login exitoso');
+    console.log(user);
+    console.log(pass);
+
+    base_login.forEach(el => {
+        console.log(el);
+
+    if (user == el.usuario && pass == el.contraseña){
         switch_login=true;
+        alert('Login exitoso');
         location.reload();
         login1();
     }else{
         alert('Usuario o contraseña incorrectos')
     }
-
+});
 }
 
 function login1(){
     if (switch_login == true){
-        
+        // alert('Login Exitoso')
     }
 }
